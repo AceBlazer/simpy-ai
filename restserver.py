b916882c795b7582d4ada87ff4feac8c6c6588cd
JasserSaanoun <diesel_1994@live.fr>
2020-05-24 02:17:59 +0100

    send email when index ok/ko

--- restserver.py
+++ restserver.py
@@ -5,6 +5,13 @@ import os
 import datetime
 from search import run
 from index import indexNow
+import pycron
+import time
+import requests
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import os
 
 
 app = Flask(__name__)
@@ -17,6 +24,7 @@ def index():
         pathIndex = os.path.join("indexes", "project_name")
         pathIndex = os.path.join(pathIndex, "customer_name")
         pathIndex = os.path.join(pathIndex, "index.csv")
+        reciever = "saanoun.jasser21@gmail.com"
         if not os.path.exists(os.path.dirname(pathIndex)):
                 try:
                     os.makedirs(os.path.dirname(pathIndex))
@@ -24,8 +32,10 @@ def index():
                     if exc.errno != errno.EEXIST:
                         raise
         indexNow("dataset", pathIndex)
+        sendEmail(reciever)
         return ("Images indexed successfully.")
     except:
+        sendErrorEmail(reciever)
         return Response("{'error':'Error occured when indexing your images.'}", status=500, mimetype='application/json')
 
 
@@ -59,3 +69,66 @@ def search():
              return Response("{'error':'Please check the image you sent.'}", status=500, mimetype='application/json')
     else:
         return Response("{'error':'Please check the image you sent.'}", status=500, mimetype='application/json')
+
+
+
+
+def sendEmail(reciever):
+    try:
+        print(os.environ['GMPW'])
+        msg = MIMEMultipart()
+        msg['from'] = "customer.simpy@gmail.com"
+        msg['to'] = str(reciever)
+        password = os.environ['GMPW']
+        msg['subject'] = "Indexing complete"
+        body = "<p>The image indexing from the project: xxx is completed successfully.</p>"
+        msg.attach(MIMEText(body, "html"))
+        print(msg)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(msg['from'], str(password))
+        server.sendmail(msg['from'], msg['to'], msg.as_string())
+        server.quit()
+    except Exception as e:
+        print(e)
+
+
+def sendErrorEmail(reciever):
+    try:
+        print(os.environ['GMPW'])
+        msg = MIMEMultipart()
+        msg['from'] = "customer.simpy@gmail.com"
+        msg['to'] = str(reciever)
+        password = os.environ['GMPW']
+        msg['subject'] = "Indexing problem"
+        body = "<p>It appears to be that the image indexing for the project: xxx got an error and has not completed.</p><p>We will check that as soon as possible.</p>"
+        msg.attach(MIMEText(body, "html"))
+        print(msg)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(msg['from'], str(password))
+        server.sendmail(msg['from'], msg['to'], msg.as_string())
+        server.quit()
+    except Exception as e:
+        print(e)
+
+'''
+#to leave untill later time bc crons/schedules are hard in windows
+@app.route('/start_cron', methods=['POST'])
+def startCron():
+    '''
+    
+
+
+'''
+@app.route('/stop_cron', methods=['POST'])
+def stopCron(): '''
+
+
+
+
+
